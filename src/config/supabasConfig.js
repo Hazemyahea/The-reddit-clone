61ff26d587095a3c37e8f113177ffba167b2de76
Hazemyahea <hazemyahea1@gmail.com>
2024-05-06 23:30:42 +0300

    test

--- src/config/supabasConfig.js
+++ src/config/supabasConfig.js
@@ -1,5 +1,4 @@
 import { createClient } from "@supabase/supabase-js";
-const supabaseUrl = process.env.VITE_API_URL;
-const supabaseKey = process.env.VITE_API_KEY;
-console.log(supabaseUrl);
+const supabaseUrl = import.meta.env.VITE_API_URL;
+const supabaseKey = import.meta.env.VITE_API_KEY;
 export const supabase = createClient(supabaseUrl, supabaseKey);
